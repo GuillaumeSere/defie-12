7e9080d88d7d005c82bfb4db78b9069eb529887a
Guillaume Sere <guillaumesere@hotmail.fr>
2023-04-14 14:45:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import dice from './images/icon-dice.svg'
+import divider1 from './images/pattern-divider-desktop.svg'
+import divider2 from './images/pattern-divider-mobile.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [advice, setAdvice] = useState({ id: null, text: "" });
+
+    const handleClick = () => {
+        fetch("https://api.adviceslip.com/advice")
+            .then((response) => response.json())
+            .then((data) => setAdvice({ id: data.slip.id, text: data.slip.advice }))
+            .catch((error) => console.log(error));
+    };
+
+    return (
+        <div className="card">
+            {advice.text && <p>ADVICE  #{advice.id}</p>}
+            {advice.text && <q>{advice.text}</q>}
+            <picture className="divider">
+                <source media="(max-width: 767px)" srcset={divider2}/>
+                    <img src={divider1} />
+            </picture>
+            <button onClick={handleClick}>
+                <img src={dice} alt="dice" />
+            </button>
+        </div>
+    );
 }
 
 export default App;
