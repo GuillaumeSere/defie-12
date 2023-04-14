9af35f7ba7d186e8b1b9de84e5282092d7c48970
Guillaume Sere <guillaumesere@hotmail.fr>
2023-04-14 14:59:56 +0200

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import dice from './images/icon-dice.svg'
 import divider1 from './images/pattern-divider-desktop.svg'
@@ -8,11 +8,19 @@ function App() {
 
     const [advice, setAdvice] = useState({ id: null, text: "" });
 
-    const handleClick = () => {
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
+
+    const fetchAdvice = () => {
         fetch("https://api.adviceslip.com/advice")
             .then((response) => response.json())
             .then((data) => setAdvice({ id: data.slip.id, text: data.slip.advice }))
             .catch((error) => console.log(error));
+    };
+
+    const handleClick = () => {
+        fetchAdvice();
     };
 
     return (
@@ -20,8 +28,8 @@ function App() {
             {advice.text && <p>ADVICE  #{advice.id}</p>}
             {advice.text && <q>{advice.text}</q>}
             <picture className="divider">
-                <source media="(max-width: 767px)" srcset={divider2}/>
-                    <img src={divider1} />
+                <source media="(max-width: 767px)" srcset={divider2} />
+                <img src={divider1} />
             </picture>
             <button onClick={handleClick}>
                 <img src={dice} alt="dice" />
